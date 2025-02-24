da6457a6dce61a24e03b9d8c0d465b4d142d0506
Phelipe <phelipe.silva@sptech.school>
2025-02-24 20:13:29 -0300

    aula03 - exemplo lista (update)

---
+++ exemplo-lista/src/main/java/school/sptech/ExemploLista.java
@@ -0,0 +1,47 @@
+package school.sptech;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExemploLista {
+    public static void main(String[] args) {
+        System.out.println("----------- Exemplo Lista -----------");
+
+        List<Integer> lista;
+        lista = new ArrayList<>();
+
+        lista.add(100);
+        lista.add(60);
+        lista.add(1, 75);
+
+        lista.set(2, 99);
+
+        Integer valor = 75;
+
+        lista.remove(valor);
+        System.out.println(lista);
+
+        List<String> frutas = new ArrayList<>(
+                List.of("Pera", "Banana", "Tomate")
+        );
+        frutas.add("Abacate");
+        frutas.add("Morango");
+        frutas.add("Goiaba");
+        frutas.add("Limão");
+
+        frutas.remove("Abacate");
+        System.out.println(frutas);
+
+        System.out.println("-------- For aprimorado com lista --------");
+        for (String fruta : frutas) {
+            System.out.println(fruta);
+        }
+
+        System.out.println("-------- For com lista --------");
+        for (int i = 0; i < frutas.size(); i++)
+        {
+            System.out.println(frutas.get(i));
+        }
+
+    }
+}
